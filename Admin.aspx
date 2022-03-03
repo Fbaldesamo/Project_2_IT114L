7b5cfca83633d99c52ff4ed0daeaba58404342c8
Fbaldesamo <baldesamofranco@gmail.com>
2022-03-04 01:26:03 +0800

    Add project files.

---
+++ Admin.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Admin.aspx.cs" Inherits="Project_2_IT114L.Admin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            SEMINAR CODE
+            <asp:TextBox ID="TextBox1" runat="server" Width="168px"></asp:TextBox>
+            <br />
+            <br />
+            SEMINAR TITLE
+            <asp:TextBox ID="TextBox2" runat="server" Width="168px"></asp:TextBox>
+            <br />
+            <br />
+            SEMINAR DATE
+            <asp:TextBox ID="TextBox3" runat="server" Width="168px"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="SAVE" />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Saved"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Admin.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Data;
+
+namespace Project_2_IT114L
+{
+    public partial class Admin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Label1.Visible = false;
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            OleDbConnection my_con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\Others\\Documents\\Database1.accdb");
+            my_con.Open();
+            OleDbCommand o_cmd = new OleDbCommand("insert into Table1 values(@Sem_Code,@Sem_Title,@SemDB)", my_con);
+            o_cmd.Parameters.AddWithValue("@Sem_Code", TextBox1.Text.Trim());
+            o_cmd.Parameters.AddWithValue("@Sem_Title", TextBox2.Text.Trim());
+            o_cmd.Parameters.AddWithValue("@SemDB", TextBox3.Text.Trim());
+            //o_cmd.Parameters.AddWithValue("d", TextBox4.Text);
+            int i = o_cmd.ExecuteNonQuery();
+            if (i > 0)
+            {
+                Label1.Text = "Data inserted successfully";
+            }
+            my_con.Close();
+
+
+
+
+            //First_try
+            //OleDbConnection con = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\Others\\Documents\\Database1.accdb");
+
+            //con.Open();
+
+
+            //OleDbCommand cmd = new OleDbCommand();
+            //"insert into Table1 values('"+TextBox1.Text+ "','" + TextBox2.Text + "'," + TextBox3.Text + ")",con;
+            //int r = cmd.ExecuteNonQuery();
+
+            //con.Close();
+            //if (r > 0)
+            //{
+            //    Label1.Visible = true;
+
+
+            //}
+
+
+        }
+    }
+}
